e4af81cffa8dab8e166629b4501e1f1b0ffa0fc4
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-06-18 15:48:42 -0500

    Added help, and notea bout MSVC

--- configure.sh
+++ configure.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-# Use: configure.sh [TARGET_TYPE]
-#
-#      TARGET_TYPE is the cmake -G option
-#
-# If TARGET_TYPE is not provided, then
-#   * If $MSYSTEM is set, "MSYS Makefiles" will be used
-#   * Otherwise, no -G will be the system default
+if [[ "$1" == '-'* ]]; then
+  cat <<EOF
 
-FORCE_BUILD=
+  Use: configure.sh [TARGET_TYPE]
+  
+       TARGET_TYPE is the cmake -G option
+        Some popular choices:
+          - 'MSYS Makefiles' 
+          - 'Visual Studio 17 2022'
+          - 'Unix Makefiles'
+          - Ninja          
+  
+       If TARGET_TYPE is not provided, then the system default will be
+       used unless $MSYSTEM is set in which case 'MSYS Makefiles' will
+       be used.
+
+EOF
+exit
+fi
 
 if [ -e examples -a -e lib -a -e CMakeLists.txt ] ; then
-  if [ -n "$FORCE_BUILD" -a -e build ] ; then
-    echo "The 'build' directory already exists.  Remove it, and run this script again."
+  if [ -e build ] ; then
+    echo 'The "build" directory already exists.  Remove it, and run this script again.'
   else
     mkdir build
     cd build
@@ -30,8 +40,10 @@ if [ -e examples -a -e lib -a -e CMakeLists.txt ] ; then
       cmake ../
     fi
 
-    echo "Now: 'cd build' & make soemthing!"
+    echo 'Now you can build with soemthing like this:'
+    echo '   cd build'
+    echo '   make bmark'
   fi
 else
-    echo "Can't find one or more of examples/, lib/, or CMakeLists.txt.  Are you in the correct directory?"
+    echo 'Can not find one or more of examples/, lib/, or CMakeLists.txt.  Are you in the correct directory?'
 fi
