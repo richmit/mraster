e6229d3ffeb144a1620e746cfba1edea2e14b542
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-08-15 18:47:56 -0500

    New: deploy_doxygen

---
+++ deploy_doxygen.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This little script publishes the doxygen documentation on the web
+#  - Make sure "PROJECT_VERSION_ID" in CMakeLists.txt is set to the tag-name
+#  - Make sure the doxygen documentation has been deployed with the correct tag-name
+#  - Make sure next-tag.org file exists for the tag comment
+
+if [ -e ~/world/my_prog/mraster/build ]; then
+  cd ~/world/my_prog/mraster/build
+  if [ -e ~/world/WWW/site/SS/mraster/ ];  then
+    make doc-lib
+    make doc-examples
+    if [ -e doc-lib/autodocs/html/index.html -a -e doc-examples/autodocs/html/index.html ]; then
+      rm -rf ~/world/WWW/site/SS/mraster/doc-lib
+      rm -rf ~/world/WWW/site/SS/mraster/doc-examples
+      mv doc-lib doc-examples/ ~/world/WWW/site/SS/mraster/
+      echo "INFO: Documentation deployed:"
+      ls -ld ~/world/WWW/site/SS/mraster/doc-*
+    else
+      echo "ERROR: Could not find generated doxygen documentation!"
+    fi
+  else
+    echo "ERROR: Could not find mraster web directory: ~/world/WWW/site/SS/mraster/"
+  fi
+else
+  echo "ERROR: Could not find mraster build directory: ~/world/my_prog/mraster/build"
+fi
