91cdac90af969c67fd213b4ee2f24724dc98b789
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-08-27 13:01:55 -0500

    Cleanup for tag

--- examples/utest_ramCanvasTpl.cpp
+++ examples/utest_ramCanvasTpl.cpp
@@ -30,6 +30,8 @@
   DAMAGE.
   @endparblock
 ********************************************************************************************************************************************************.H.E.**/
+/** @cond exj */
+
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
 #include <boost/test/unit_test.hpp>
