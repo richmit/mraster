496b0dc63e4f1ab702f2a643b3d93d6062a16e31
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-09-10 18:49:24 -0500

    Updates for new example

--- bin/check.rb
+++ bin/check.rb
@@ -3,7 +3,7 @@
 homed = '/Users/richmit';
 mrsrc = '/Users/richmit/world/my_prog/mraster'
 
-#        Thingy                                              Depends on Thingy
+#        Thingy                                              This Depends on Thingy
 deps = { "#{mrsrc}/examples/BurningShip.cpp"              => "#{homed}/world/WWW/site/SS/BurningShip/index.org",
          "#{mrsrc}/examples/apollony.cpp"                 => "#{homed}/world/WWW/site/SS/AGasket/index.org",
          "#{mrsrc}/examples/BurningShip.cpp"              => "#{homed}/world/WWW/site/SS/BurningShip/index.org",
@@ -12,6 +12,8 @@ deps = { "#{mrsrc}/examples/BurningShip.cpp"              => "#{homed}/world/WWW
          "#{mrsrc}/examples/mandelbrot_potential_b_0.pov" => "#{homed}/world/WWW/site/SS/mandelbrot/index.org",
          "#{mrsrc}/examples/newton_bm_cplx.cpp"           => "#{homed}/world/WWW/site/SS/newton/index.org",
          "#{mrsrc}/examples/newton_max_mod.cpp"           => "#{homed}/world/WWW/site/SS/newton/index.org",
+         "#{mrsrc}/examples/multibrotMovie.cpp"           => "#{homed}/world/WWW/site/SS/multibrot/index.org",
+         "#{mrsrc}/examples/multibrotSnaps.cpp"           => "#{homed}/world/WWW/site/SS/multibrot/index.org",
          "#{mrsrc}/README.org"                            => "#{mrsrc}/docs/index.org",
          "#{mrsrc}/lib/hello.cpp"                         => "#{mrsrc}/docs/QuickStart.org"
        };
