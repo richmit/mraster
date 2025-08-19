be5dc1878a119c8c6833a60dc93a887443d33d83
Mitch Richling <11151403+richmit@users.noreply.github.com>
2025-08-19 15:35:50 -0500

    Added 3da_frac_lorenz example

---
+++ examples/3da_frac_lorenz.cpp
@@ -0,0 +1,111 @@
+// -*- Mode:C++; Coding:us-ascii-unix; fill-column:158 -*-
+/*******************************************************************************************************************************************************.H.S.**/
+/**
+ @file      3da_frac_lorenz.cpp
+ @author    Mitch Richling <https://www.mitchr.me>
+ @brief     Draw lorenz attractors with different initial conditions.@EOL
+ @std       C++20
+ @copyright
+  @parblock
+  Copyright (c) 1988-2015, Mitchell Jay Richling <https://www.mitchr.me> All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+  DAMAGE.
+  @endparblock
+ @filedetails
+
+  This code generates a fractal-like image from the Lorenz system.  The image represents a rectangle in @f$\mathbb{R}^3@f$ parallel to the @f$x@f$-@f$z@f$
+  plane, and hitting the @f$y@f$ axis at @f$1.51@f$. We start the IVP problem with initial conditions in this rectangle.  The color represents the time
+  required for the @f$x@f$ value to change sign.
+
+  For reference the Lorenz system is defined by:
+  @f[ \begin{array}{lcl}
+        \frac{dx}{dt} & = & a(y-x)   \\
+        \frac{dy}{dt} & = & x(b-z)-y \\
+        \frac{dz}{dt} & = & xy-cz
+  \end{array} @f]
+
+  Traditional parameter values are:
+
+  @f[ \begin{array}{lcc}
+        a & = & 10   \\
+        b & = & 28 \\
+        c & = & \frac{8}{3}
+  \end{array} @f]
+
+*/
+/*******************************************************************************************************************************************************.H.E.**/
+/** @cond exj */
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <valarray>
+#include "ramCanvas.hpp"
+#include "MRMathSFUN.hpp"
+
+typedef mjr::ramCanvas1c16b drc_t;
+typedef mjr::color3c8b oc_t;
+typedef mjr::ramCanvasPixelFilter::FuncHomoTransform<drc_t, oc_t> hpf_t;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+int main(void) {
+  std::chrono::time_point<std::chrono::system_clock> startTime = std::chrono::system_clock::now();
+  const int XSIZ = 7680/1;
+  const int YSIZ = 4320/1;
+
+  drc_t theRamCanvas(XSIZ, YSIZ, -200, 200, 10, 40);
+
+  const drc_t::colorChanType maxPtsPerCurve = drc_t::colorType::maxChanVal;
+  const drc_t::coordFltType tDelta          = 0.001;
+  const drc_t::coordFltType a               = 10.0;
+  const drc_t::coordFltType b               = 28.0;
+  const drc_t::coordFltType c               = 8.0 / 3;
+
+# pragma omp parallel for schedule(static,1)
+  for(drc_t::coordIntType yi=0;yi<theRamCanvas.getNumPixY();yi++) {
+    std::cout << "Line: " << yi << std::endl;
+    for(drc_t::coordIntType xi=0;xi<theRamCanvas.getNumPixX();xi++) {
+      drc_t::coordFltType xNew, yNew, zNew;
+      drc_t::coordFltType x, y, z;
+      x = theRamCanvas.int2realX(xi);
+      y = 1.51;
+      z = theRamCanvas.int2realY(yi);
+      int s = mjr::math::sfun::sgn(x);
+      for(drc_t::colorChanType i=0;i<maxPtsPerCurve;i++) {
+        xNew = x + a * (y - x) * tDelta;
+        yNew = y + (x * (b - z) - y) * tDelta;
+        zNew = z + (x * y - c * z) * tDelta;
+        if (mjr::math::sfun::sgn(xNew) != s) {
+          theRamCanvas.drawPoint(xi, yi, i);
+          break;
+        }
+        x=xNew;
+        y=yNew;
+        z=zNew;
+      }
+    }
+  }
+  theRamCanvas.writeRAWfile("3da_frac_lorenz.mrw");
+
+  /* Write with color map */
+  theRamCanvas.writeTIFFfile("3da_frac_lorenz.tiff", hpf_t(theRamCanvas,[](auto inColor) { return oc_t::csCCfractal0RYBCW::c(inColor.getC0()); }));
+
+  std::chrono::duration<double> runTime = std::chrono::system_clock::now() - startTime;
+  std::cout << "Total Runtime " << runTime.count() << " sec" << std::endl;
+  return 0;
+}
+/** @endcond */
+
